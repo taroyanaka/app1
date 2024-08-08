a7c92797a744e05b00e84b0cd378abdce0c0c001
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-08-08 11:15:28 +0900

    a

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [svelte()],
-})
+  // base: '/your-repo-name/', // リポジトリ名に置き換えてください
+  base: 'https://taroyanaka.github.io/app1', // リポジトリ名に置き換えてください
+  plugins: [svelte()]
+});
