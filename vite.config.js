06243fd3649fe85c77a4609631f2819813b6d57f
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-08-08 11:46:52 +0900

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // GitHub Pagesのリポジトリのサブディレクトリでホストする場合のベースパス設定
 export default defineConfig({
   plugins: [svelte()],
-  base: '/app1/', // リポジトリ名に置き換えてください
+  base: '/', // リポジトリ名に置き換えてください
   build: {
-    outDir: '.', // ルートディレクトリに出力
+    outDir: './dist/', // ルートディレクトリに出力
     rollupOptions: {
       input: {
         main: 'index.html' // ルートのindex.htmlを入力ファイルに指定
