17d176c4c0d833c680b26e537b504926007256d9
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-08-08 11:26:06 +0900

    a

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import svelte from '@sveltejs/vite-plugin-svelte';
 
-// https://vitejs.dev/config/
-
+// GitHub Pagesのルートでホストする場合のベースパス設定
 export default defineConfig({
-  // base: '/your-repo-name/', // リポジトリ名に置き換えてください
-//  base: 'https://taroyanaka.github.io/app1/', // リポジトリ名に置き換えてください
-  base: '/app1/', // リポジトリ名に置き換えてください
-  plugins: [svelte()]
+  base: '/', // ルートパス
+  plugins: [svelte()],
+  build: {
+    outDir: 'dist' // デフォルトのビルド出力先
+  }
 });
